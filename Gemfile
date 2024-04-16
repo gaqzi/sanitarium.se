98b3d05142e69c048f663b3b93a002977d753728
Björn Andersson <ba@sanitarium.se>
2024-04-16 12:31:59 +0800

    Install SEO plugin and update banner generator
    The banner generator was made very hackily and didn't properly
    integrate with Jekyll, now it actually generates the banners
    and make them known at creation time. And it populates the information
    so it's available to other plugins.
    
    The SEO plugin provides the tags I set before and more, so it provides
    more than I used to have with less maintenance.

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ group :jekyll_plugins do
   gem 'jekyll-feed'
   gem 'jekyll-paginate'
   gem 'jekyll-sitemap'
+  gem 'jekyll-seo-tag'
 #  gem 'jekyll-picture-tag'
 end
 
--- _plugins/banner_creator.rb
+++ _plugins/banner_creator.rb
@@ -5,14 +5,7 @@ require 'tempfile'
 require 'webrick'
 
 module Jekyll
-  module RenderBannerFilter
-    def render_banner(page)
-      banner = Banner.new(page)
-      banner.create! unless banner.exists?
-
-      "/#{banner.path}"
-    end
-
+  module BannerGenerator
     class Banner
       def initialize(page)
         @page = page
@@ -31,7 +24,11 @@ module Jekyll
       end
 
       def path
-        "#{banner_path}/#{@page['slug']}--#{checksum}.gen.png"
+        "#{dir}/#{name}"
+      end
+
+      def name
+        "#{@page['slug']}--#{checksum}.gen.png"
       end
 
       def phantomjs_command(input_file, output_file, dimensions = '1200px*630px')
@@ -40,9 +37,9 @@ module Jekyll
 
       def create!
         template = Liquid::Template.parse(File.open(template_file).read)
-        # file = Tempfile.new([checksum, '.html'])
         dir = Dir.mktmpdir("banner")
         file = File.new(File.join(dir, "#{checksum}.html"), "w+")
+        puts @page
         file.write(template.render(@page))
         file.close
         puts "banner: #{@page['title']} - #{file.path} - #{path}"
@@ -57,7 +54,7 @@ module Jekyll
         server_path = ERB::Util.url_encode("http://jekyll:3001/#{checksum}.html")
 
         # assume this will be running in docker compose since we need the screenshotting service now
-        uri = URI("http://ws-screenshot:3000/api/screenshot?resX=1200&resY=630&outFormat=png&waitTime=150&isFullPage=false&dismissModals=false&url=#{server_path}")
+        uri = URI("http://ws-screenshot:3000/api/screenshot?resX=#{width}&resY=#{height}&outFormat=png&waitTime=150&isFullPage=false&dismissModals=false&url=#{server_path}")
         Net::HTTP.start(uri.host, uri.port) do |http|
           http.open_timeout = 0.5 # how long to wait for the docker network to be available
           http.read_timeout = 40 # how long we'll wait for a response in total
@@ -76,11 +73,19 @@ module Jekyll
         webrick.kill
       end
 
-      private
-
-      def banner_path
+      def dir
         'img/banners'
       end
+
+      def height
+        630
+      end
+
+      def width
+        1200
+      end
+
+      private
 
       def template_file
         '_layouts/banner.html'
@@ -94,7 +99,31 @@ module Jekyll
         '_rasterize.js'
       end
     end
+
+    class Generator < Jekyll::Generator
+      safe true
+      priority :highest
+
+      def generate(site)
+        site.posts.docs.each do |post|
+          image_path = post.data.dig('image', 'path')
+          next if !image_path.nil? && !image_path.empty?
+
+          banner = Banner.new(post.data)
+
+          # Create the banner and add it to Jekyll so it gets copied to the right place
+          unless banner.exists?
+            banner.create!
+            site.static_files << Jekyll::StaticFile.new(site, site.source, banner.dir, banner.name)
+          end
+
+          # Add to the frontmatter so other plugins find it
+          post.data['image'] ||= {}
+          post.data['image']['path'] = "/#{banner.path}"
+          post.data['image']['height'] = banner.height
+          post.data['image']['width'] = banner.width
+        end
+      end
+    end
   end
 end
-
-Liquid::Template.register_filter(Jekyll::RenderBannerFilter)
