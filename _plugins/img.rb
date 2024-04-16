94ed825f0f07a5f29b96c4207daabbdbe240b41f
Björn Andersson <ba@sanitarium.se>
2024-04-16 14:15:30 +0800

    Lazy load post images by default

--- _plugins/img.rb
+++ _plugins/img.rb
@@ -1,6 +1,7 @@
 module Jekyll
   class ImageTag < Liquid::Tag
     ALT_TEXT = /(alt="([^"]+)")/
+    LAZY = /(lazy=(true|false))/
 
     def initialize(tag_name, text, parse_context)
       super
@@ -9,12 +10,17 @@ module Jekyll
         @alt = "alt=\"#{$2}\" title=\"#{$2}\""
         text.slice!($1)
       end
+      @lazy = 'loading="lazy"'
+      if text =~ LAZY
+        @lazy = '' if $2 != 'true'
+        text.slice!($1)
+      end
 
       @src = text.strip
     end
 
     def render(context)
-      "<img class=\"center-block img-responsive\" #{@alt} src=\"#{@src}\">"
+      "<img class=\"center-block img-responsive\" #{@lazy} #{@alt} src=\"#{@src}\">"
     end
   end
 end
